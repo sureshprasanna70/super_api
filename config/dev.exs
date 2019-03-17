02ca4ea012bbb0ceea19ce98cfaedd177a20e81e
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-03-17 14:24:32 +0530

    dev conf

--- config/dev.exs
+++ config/dev.exs
@@ -39,8 +39,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :super_api, SuperApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DEVELOPMENT_DATABASE_USERNAME"),
+  password: System.get_env("DEVELOPMENT_DATABASE_PASSWORD"),
   database: "super_api_dev",
   hostname: "localhost",
   pool_size: 10
